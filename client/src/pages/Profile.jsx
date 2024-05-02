ce941ae2e837ed91925a348d6f5d65dd4bda2a0a
marjan <marjan199721@gmail.com>
2024-05-02 22:32:12 +0200

    upload profile image

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,17 +1,86 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-
+import { app } from "../firbase.config";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  // eslint-disable-next-line no-unused-vars
+  let [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // eslint-disable-next-line no-const-assign
+        setFilePerc = Math.round(progress);
+      },
+      // eslint-disable-next-line no-unused-vars
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      }
+    );
+  };
+
   return (
-    <div className=" flex flex-col p-3 max-w-lg mx-auto">
-      <img
-        className=" rounded-full object-cover my-6 outline-dotted outline-indigo-700 cursor-pointer self-center  w-24 h-24"
-        src={currentUser.avatar}
-      />
-      <h1 className="font-semibold text-xl text-center  ">
-        {currentUser.username}
-      </h1>
+    <div className=" p-3 max-w-lg mx-auto">
       <form className=" flex flex-col justify-center gap-3   my-7 ">
+      <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          className=" rounded-full object-cover my-6 outline-dotted outline-indigo-700 cursor-pointer self-center  w-24 h-24"
+          src={formData.avatar||currentUser.avatar}
+        />
+ <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully uploaded!</span>
+          ) : (
+            ''
+          )}
+        </p>
+
+        <h1 className="font-semibold text-xl text-center  ">
+          {currentUser.username}
+        </h1>
+
+
         <input
           type="text"
           placeholder="username"
@@ -30,10 +99,14 @@ const Profile = () => {
           className="border p-3 rounded border-indigo-700"
           id="password"
         />
-        <button className="uppercase rounded-lg p-4 bg-indigo-600 hover:opacity-90 disabled:opacity-75 text-white">Update</button>
+        <button className="uppercase rounded-lg p-4 bg-indigo-600 hover:opacity-90 disabled:opacity-75 text-white">
+          Update
+        </button>
       </form>
       <div className="flex justify-between text-white items-center">
-        <span className="bg-red-500 p-1 rounded-md text-[12px]">Delete Account</span>
+        <span className="bg-red-500 p-1 rounded-md text-[12px]">
+          Delete Account
+        </span>
         <span className="bg-red-500 p-1 rounded-md text-[12px]">Sign Out</span>
       </div>
     </div>
